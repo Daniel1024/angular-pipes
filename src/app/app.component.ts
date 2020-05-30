7bef0f046cb327c60082f12fc775a43606985709
Daniel Lopez <ext_delopezp@falabella.cl>
2020-05-29 20:59:20 -0400

    pipe personalisado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pipes';
   nombre: string = 'Capitán América';
+  nombre2: string = 'dAnIel lÓpeZ';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario: number = 1234.5;
   fecha: Date = new Date();
+  idioma: string = 'es';
 
   valorPromesa = new Promise<string>(resolve => {
     setTimeout(() => {
@@ -28,5 +30,9 @@ export class AppComponent {
       calle: 'Primera',
       casa: 20
     }
+  }
+
+  cambiarIdioma(idioma: string) {
+    this.idioma = idioma;
   }
 }
